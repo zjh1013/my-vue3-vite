594d21a63821a18aef2825a369b6789f993bb6aa
zuojiahui <zuo2681731956@163.com>
2022-06-05 16:03:55 +0800

    '配置路由'

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,8 +1,16 @@
 const routes = [
     {
+        path:'/',
+        redirect:'/pages'
+    },
+    {
         path: '/index',
         component: () => import('@view/index.vue')
     },
+    {
+        path:'/pages',
+        component:() => import('@view/putopage/index.vue')
+    }
 ]
 
 export default routes
