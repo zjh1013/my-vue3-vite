62c8b42d29d63da0f3807514290b572f7983d75a
ningli <xiaojingang987@163.com>
2022-06-21 15:52:25 +0800

    修改

--- src/view/putopage/index.vue
+++ src/view/putopage/index.vue
@@ -5,20 +5,23 @@
         <div style="color: aqua">父组件</div>
         <div>子组件对我说：{{ children_msg }}</div>
         <children :title="msg" @listen="listenToChildren"></children>
-        {{ value }}
+        {{  children_msg.value }}
       </div>
       <div class="big_box_right">
-        <div class="big_box_right_ones">
-           <div>子组件说的话：{{nowChir_l}}</div> 
-            <Childe1 :childers1="klj" @listen1='newtalkenChilde'></Childe1>
-        </div>
+        <div class="big_box_right_ones"></div>
         <div class="big_box_right_ones">
           <div class="big_box_right_ones_chick">
-              <p v-for="(item,index) in arrALL"  :key="index"><input type="checkbox" @change="checkalls(item,index)" >{{item.name}}</p>
+            <p v-for="(item, index) in arrALL" :key="index">
+              <input type="checkbox" @change="checkalls(item, index)" />{{
+                item.name
+              }}
+            </p>
           </div>
         </div>
         <div class="big_box_right_ones">
-          <p  v-for="(item,index) in stata.arresLL"  :key="index"><input type="checkbox" />{{item.name}}</p>
+          <p v-for="(item, index) in stata.arresLL" :key="index">
+            <input type="checkbox" />{{ item.name }}
+          </p>
         </div>
         <div class="big_box_right_ones"></div>
         <div class="big_box_right_ones"></div>
@@ -31,64 +34,56 @@
 </template>
 
 <script>
-import children from "./child.vue";
-import Childe1 from './childe1.vue'
-import { defineComponent,reactive, ref } from "vue"
+import children from "./components/child.vue";
+import { defineComponent, reactive, ref } from "vue";
 export default defineComponent({
   components: {
     children,
-    Childe1
   },
   name: "father",
   setup() {
-    let msg = "我是父组件"
-    let children_msg = ref("") // ref的作用是实现响应式， 如果没有ref则不能实现响应式（引用数据类型用reactive）
+    let msg = "我是父组件";
+    let children_msg = ref(""); // ref的作用是实现响应式， 如果没有ref则不能实现响应式（引用数据类型用reactive）
     let listenToChildren = (val) => {
-      children_msg.value = val // 使用ref包裹的数据，需要通过.value的形式访问他的值
-    }
-    let klj = "我是父组件"
-    let nowChir_l = ref("")
-    let newtalkenChilde =(val)=>{
-        nowChir_l.value = val
-    }
-    let checkAll = ref(false)
-    let arrALL = [{ name: "法拉利", isCheck: false },{ name: "兰博基尼", isCheck: false },{ name: "柯尼塞格", isCheck: false },]
+      children_msg.value = val; // 使用ref包裹的数据，需要通过.value的形式访问他的值
+    };
+    let checkAll = ref(false);
+    let arrALL = [
+      { name: "法拉利", isCheck: false },
+      { name: "兰博基尼", isCheck: false },
+      { name: "柯尼塞格", isCheck: false },
+    ];
     const stata = reactive({
-      arresLL: []
-    })
-    // let arresLL = [];
-   let checkalls = (a,b)=>{
-        a.isCheck = a.isCheck ? false : true
-        console.log( a.isCheck)
-        if(a.isCheck){
-          stata.arresLL.push(a)
-        }else{
-           stata.arresLL.pop(a)
-        }
-        console.log(stata.arresLL)
-}
+      arresLL: [],
+    });
+    let checkalls = (a, b) => {
+      a.isCheck = a.isCheck ? false : true;
+      console.log(a.isCheck);
+      if (a.isCheck) {
+        stata.arresLL.push(a);
+      } else {
+        stata.arresLL.pop(a);
+      }
+      console.log(stata.arresLL);
+    };
     return {
       msg,
-      klj,
       children_msg,
-      nowChir_l,
       checkAll,
       arrALL,
-      // arresLL,
       stata,
       listenToChildren,
-      newtalkenChilde,
-      checkalls
-    }
+      checkalls,
+    };
   },
-})
+});
 </script> 
 
 <style lang="less" scoped>
 .box {
   width: 100%;
 }
-.big_box {  
+.big_box {
   width: 95%;
   // height: 660px;
   background: rgba(64, 150, 219, 0.533) 00;
@@ -116,7 +111,7 @@ export default defineComponent({
       // .big_box_right_ones_chick{
 
       //   input{
-         
+
       //   }
       // }
     }
