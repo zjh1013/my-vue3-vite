79ba4550057649eef3985d56794fa8caae4b4010
ningli <xiaojingang987@163.com>
2022-06-22 14:41:43 +0800

    新增

--- src/view/putopage/components/child.vue
+++ src/view/putopage/components/child.vue
@@ -1,9 +1,8 @@
 <template>
-  <div>
-    <div style="color: brown">子组件</div>
+  <div class="childe_Back">
+    <!-- <div style="color: brown"></div> -->
     <!-- 父传子使用方法和vue2相同 -->
-    <div>父组件传过来的值为：{{ title }}</div>
-    <button @click="sayToFather">向父组件说话</button>
+    <el-button type="danger" @click="sayToFather" class="butonw">Come on</el-button>
   </div>
 </template>
 
@@ -21,9 +20,13 @@ export default defineComponent({
     // context作用是获取上下文对象，
     // 如果setup写法为setup(props, { emit })的方式的话，下面的context可以省略
     const sayToFather = () => {
-      const ask = "我是子组件，我对父组件说话";
+      // const ask = "let's go crazy";
       //   context.emit("listen", ask)
-      emit("listen", ask);
+      ElMessage({
+        message: "let's go crazy",
+        type: "success",
+      });
+      // emit("listen", ElMessage); emit 是传输对应的值，如果传入的是方法或者是对象，将字符串的形势展现出来 ，传入的本身是什么展示的就是什么
     };
     return {
       sayToFather,
@@ -31,6 +34,19 @@ export default defineComponent({
   },
 });
 </script>
-
-<style>
+<style lang="less" scoped>
+.childe_Back {
+  background: url("@assets/images/login/login_bg.png");
+  background-size: cover;
+  height: 100%;
+  position: relative;
+}
+.butonw{
+  position: absolute;
+  transform: translate(-50%,-50%);
+  top: 30%;
+  left: 50%;
+}
 </style>
+
+
